44e83ef1efbede7ccae3391ef20982472274b566
Arnab Sardar <arnabsardar@Arnabs-MacBook-Air.local>
2026-06-22 03:01:00 +0600

    Switch OCR to OCR.Space

--- note_upload/ocr_backend.py
+++ note_upload/ocr_backend.py
@@ -1,7 +1,41 @@
-import pytesseract
+import requests
+import io
+import os
+
+API_KEY = os.getenv("OCR_SPACE_API_KEY")
 
 def extract_text(image):
 
-    text = pytesseract.image_to_string(image)
+    img_bytes = io.BytesIO()
 
-    return text
+    image.save(img_bytes, format="PNG")
+
+    img_bytes.seek(0)
+
+    response = requests.post(
+        "https://api.ocr.space/parse/image",
+        files={
+            "filename": (
+                "image.png",
+                img_bytes,
+                "image/png"
+            )
+        },
+        data={
+            "apikey": API_KEY,
+            "language": "eng",
+            "isOverlayRequired": False
+        }
+    )
+
+    result = response.json()
+
+    if result.get("IsErroredOnProcessing"):
+        return "OCR Failed"
+
+    parsed = result.get("ParsedResults")
+
+    if not parsed:
+        return "No text found"
+
+    return parsed[0]["ParsedText"]
